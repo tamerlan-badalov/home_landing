a509934354470c676f3f7786a837b820ee7a8643
tamerlan-badalov <tamerlanvb@code.edu.az>
2020-02-16 20:40:48 +0400

    portfolio

---
+++ js/min.js
@@ -0,0 +1,16 @@
+$('.img_bg').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+arrows:false,
+dots:true,
+
+});
+$('.client_bg').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+arrows:false,
+dots:true,
+
+});
